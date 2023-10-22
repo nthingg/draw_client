19a64557b743fe0bab1e46f899f5a291b4b1b613
nthingg <thinhnguyenngoc1424@gmail.com>
2023-10-23 03:30:23 +0700

    init commit

---
+++ DrawClient/Pages/Customer/Course/List.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DrawClient.Pages.Customer.Course
+{
+    public class ListModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
