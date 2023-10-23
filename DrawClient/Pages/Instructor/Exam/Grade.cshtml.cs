d59f991c9f1d3464365c2391b849ccdc61ca1659
nthingg <thinhnguyenngoc1424@gmail.com>
2023-10-23 15:57:52 +0700

    ThinhNN - Base Admin, Instructor

---
+++ DrawClient/Pages/Instructor/Exam/Grade.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DrawClient.Pages.Instructor.Exam
+{
+    public class GradeModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
