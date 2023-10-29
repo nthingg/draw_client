9d9899d2f7fd3ead6f482db6e56e227a9e6b9480
QuocManh2002 <manhnq2002@gmail.com>
2023-10-30 01:47:11 +0700

    update 30/10

--- DrawClient/Pages/Admin/Customer/List.cshtml.cs
+++ DrawClient/Pages/Admin/Customer/List.cshtml.cs
@@ -1,10 +1,19 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using ViewModel.Instructor;
 
 namespace DrawClient.Pages.Admin.Customer
 {
     public class ListModel : PageModel
     {
+        Uri baseAddress = new Uri("http://localhost:5173/api/");
+        private HttpClient _httpClient;
+        public ICollection<InstructorBaseViewModel> Instructors { get; set; }
+        public ListModel()
+        {
+            _httpClient = new HttpClient();
+            Instructors = new List<InstructorBaseViewModel>();
+        }
         public void OnGet()
         {
         }
