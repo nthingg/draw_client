bc793b89562e419bea3866d5a07289edc94be9d7
QuocManh2002 <manhnq2002@gmail.com>
2023-10-30 15:43:13 +0700

    admin ins

---
+++ DrawClient/Pages/Admin/Instructor/Detail.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DrawClient.Pages.Admin.Instructor
+{
+    public class DetailModel : PageModel
+    {
+        public void OnGetAsync(int id)
+        {
+
+        }
+    }
+}
