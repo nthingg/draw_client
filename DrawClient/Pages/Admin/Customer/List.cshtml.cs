2235808691bd01bfcdf7bb8014d554a3228d5346
QuocManh2002 <manhnq2002@gmail.com>
2023-10-31 02:27:06 +0700

    update admin

--- DrawClient/Pages/Admin/Customer/List.cshtml.cs
+++ DrawClient/Pages/Admin/Customer/List.cshtml.cs
@@ -1,6 +1,9 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json;
+using ViewModel.Base;
 using ViewModel.Instructor;
+using ViewModel.Learner;
 
 namespace DrawClient.Pages.Admin.Customer
 {
@@ -8,14 +11,23 @@ namespace DrawClient.Pages.Admin.Customer
     {
         Uri baseAddress = new Uri("http://localhost:5173/api/");
         private HttpClient _httpClient;
-        public ICollection<InstructorBaseViewModel> Instructors { get; set; }
+        public ICollection<LearnerBaseViewModel> Learners { get; set; }
+        public Page<LearnerBaseViewModel> page { get; set; }
         public ListModel()
         {
             _httpClient = new HttpClient();
-            Instructors = new List<InstructorBaseViewModel>();
+            Learners = new List<LearnerBaseViewModel>();
         }
-        public void OnGet()
+        public async Task<IActionResult> OnGetAsync()
         {
+            HttpResponseMessage response = _httpClient.GetAsync(baseAddress + "learner/page?pageIndex=0&pageSize=10").Result;
+            if (response.IsSuccessStatusCode)
+            {
+                string data = response.Content.ReadAsStringAsync().Result;
+                var rs = JsonConvert.DeserializeObject<Page<LearnerBaseViewModel>>(data);
+                Learners = rs.Items;
+            }
+            return Page();
         }
     }
 }
--- DrawClient/Pages/Admin/Instructor/Detail.cshtml.cs
+++ DrawClient/Pages/Admin/Instructor/Detail.cshtml.cs
@@ -1,13 +1,29 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json;
+using ViewModel.Instructor;
 
 namespace DrawClient.Pages.Admin.Instructor
 {
     public class DetailModel : PageModel
     {
-        public void OnGetAsync(int id)
+        Uri baseAddress = new Uri("http://localhost:5173/api/");
+        private HttpClient _httpClient;
+        public InstructorViewModel instructor { get; set; }
+        public DetailModel()
         {
+            _httpClient = new HttpClient();
 
+        }
+        public async Task<IActionResult> OnGetAsync(int id)
+        {
+            HttpResponseMessage response = _httpClient.GetAsync(baseAddress + "instructor/"+id).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                string data = response.Content.ReadAsStringAsync().Result;
+                instructor = JsonConvert.DeserializeObject<InstructorViewModel>(data);
+            }
+            return Page();
         }
     }
 }
