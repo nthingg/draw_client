23290850e29c80abd057122f0b8cb4fe99af74eb
nthingg <thinhnguyenngoc1424@gmail.com>
2023-10-31 02:02:35 +0700

    ThinhNN - Customer course list add

--- DrawClient/Pages/Customer/Course/List.cshtml.cs
+++ DrawClient/Pages/Customer/Course/List.cshtml.cs
@@ -1,12 +1,42 @@
+using DrawClient.Helper;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json;
+using System.Net.Http;
+using ViewModel.Base;
+using ViewModel.Course;
 
 namespace DrawClient.Pages.Customer.Course
 {
     public class ListModel : PageModel
     {
-        public void OnGet()
+        private readonly IConfiguration _configuration;
+        private readonly HttpClient _client;
+
+        public ListModel(IConfiguration configuration)
         {
+            _configuration = configuration;
+            _client = new HttpClient();
+            var apiUrl = _configuration.GetSection("ApiUrl").Get<string>();
+            _client.BaseAddress = new Uri(apiUrl);
+        }
+
+        public Page<CourseViewModel> Courses { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int pageIndex = 0)
+        {
+            var request = new HttpRequestMessage(HttpMethod.Get, _client.BaseAddress + $"/course/page?pageIndex={pageIndex}&pageSize={5}");
+            var res = await _client.SendAsync(request);
+            if (res.IsSuccessStatusCode)
+            {
+                var dataStr = await res.Content.ReadAsStringAsync();
+                var courses = JsonConvert.DeserializeObject<Page<CourseViewModel>>(dataStr);
+                if (courses is not null)
+                {
+                    Courses = courses;
+                }
+            }
+            return Page();
         }
     }
 }
