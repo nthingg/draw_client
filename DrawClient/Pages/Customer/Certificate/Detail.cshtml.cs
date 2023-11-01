a6adc97cf838ea6f7874e131bb88d2064e2cc2d3
nthingg <thinhnguyenngoc1424@gmail.com>
2023-11-02 01:58:22 +0700

    ThinhNN - Add certificate section

--- DrawClient/Pages/Customer/Certificate/Detail.cshtml.cs
+++ DrawClient/Pages/Customer/Certificate/Detail.cshtml.cs
@@ -1,16 +1,70 @@
+using DrawchadViewModel.Course;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json;
+using ViewModel.Learner;
 
-namespace DrawClient.Pages.Customer.Exam
+namespace DrawClient.Pages.Customer.Certificate
 {
     public class DetailModel : PageModel
     {
-        [BindProperty]
-        public string ImageURl { get; set; }
+        private readonly IConfiguration _configuration;
+        private readonly HttpClient _client;
 
-        public void OnGet()
+        public DetailModel(IConfiguration configuration)
         {
-            ImageURl = "https://i.pinimg.com/originals/c3/ac/09/c3ac09a329ca1a4dbbbb8620a26a57ca.jpg";
+            _configuration = configuration;
+            _client = new HttpClient();
+            var apiUrl = _configuration.GetSection("ApiUrl").Get<string>();
+            _client.BaseAddress = new Uri(apiUrl);
+        }
+
+        public List<PurchasedCourseViewModel> Purchased { get; set; }
+
+        public PurchasedCourseViewModel Certificate { get; set; }
+
+        public LearnerBaseViewModel Learner { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int id)
+        {
+            var token = HttpContext.Session.GetString("learnerToken");
+            var request = new HttpRequestMessage(HttpMethod.Get, _client.BaseAddress + "/course/purchased-course");
+            request.Headers.Add("Authorization", $"Bearer {token}");
+
+            var res = await _client.SendAsync(request);
+
+            if (res.IsSuccessStatusCode)
+            {
+                var dataStr = await res.Content.ReadAsStringAsync();
+                var courses = JsonConvert.DeserializeObject<List<PurchasedCourseViewModel>>(dataStr);
+                if (courses is not null)
+                {
+                    Purchased = courses;
+                }
+
+                Certificate = Purchased.FirstOrDefault(c => c.Id == id);
+                await GetLearner();
+            }
+            return Page();
+        }
+
+        private async Task GetLearner()
+        {
+            var id = HttpContext.Session.GetString("learnerId");
+
+            var request = new HttpRequestMessage(HttpMethod.Get, _client.BaseAddress + "/learner/" + id);
+
+            var res = await _client.SendAsync(request);
+
+            if (res.IsSuccessStatusCode)
+            {
+                var dataStr = await res.Content.ReadAsStringAsync();
+                var learner = JsonConvert.DeserializeObject<LearnerBaseViewModel>(dataStr);
+                if (learner is not null)
+                {
+                    Learner = learner;
+                }
+            }
         }
     }
 }
--- DrawClient/Pages/Customer/Certificate/List.cshtml.cs
+++ DrawClient/Pages/Customer/Certificate/List.cshtml.cs
@@ -1,12 +1,45 @@
+using DrawchadViewModel.Course;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json;
+using ViewModel.Course;
 
-namespace DrawClient.Pages.Customer.Exam
+namespace DrawClient.Pages.Customer.Certificate
 {
     public class ListModel : PageModel
     {
-        public void OnGet()
+        private readonly IConfiguration _configuration;
+        private readonly HttpClient _client;
+
+        public ListModel(IConfiguration configuration)
         {
+            _configuration = configuration;
+            _client = new HttpClient();
+            var apiUrl = _configuration.GetSection("ApiUrl").Get<string>();
+            _client.BaseAddress = new Uri(apiUrl);
+        }
+
+        public List<PurchasedCourseViewModel> Purchased { get; set; }
+
+        public async Task<IActionResult> OnGetAsync()
+        {
+            var token = HttpContext.Session.GetString("learnerToken");
+            var request = new HttpRequestMessage(HttpMethod.Get, _client.BaseAddress + "/course/purchased-course");
+            request.Headers.Add("Authorization", $"Bearer {token}");
+
+            var res = await _client.SendAsync(request);
+
+            if (res.IsSuccessStatusCode)
+            {
+                var dataStr = await res.Content.ReadAsStringAsync();
+                var courses = JsonConvert.DeserializeObject<List<PurchasedCourseViewModel>>(dataStr);
+                Purchased = new List<PurchasedCourseViewModel>();
+                if (courses is not null)
+                {
+                    Purchased.AddRange(courses.Where(course => course.HasPassed));
+                }
+            }
+            return Page();
         }
     }
 }
