ec24610a3328119ddadd28a454336d2f9a6bf70f
nthingg <thinhnguyenngoc1424@gmail.com>
2023-11-05 12:59:45 +0700

    ThinhNN - Add course section admin

--- DrawClient/Pages/Admin/Course/Detail.cshtml.cs
+++ DrawClient/Pages/Admin/Course/Detail.cshtml.cs
@@ -1,12 +1,51 @@
+using Domain.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json;
+using System.Text;
+using ViewModel.Base;
+using ViewModel.Cart;
+using ViewModel.Course;
+using ViewModel.Lesson;
+using ViewModel.Order;
+using ViewModel.Review;
 
 namespace DrawClient.Pages.Admin.Course
 {
     public class DetailModel : PageModel
     {
-        public void OnGet()
+        private readonly IConfiguration _configuration;
+        private readonly HttpClient _client;
+
+        public DetailModel(IConfiguration configuration)
         {
+            _configuration = configuration;
+            _client = new HttpClient();
+            var apiUrl = _configuration.GetSection("ApiUrl").Get<string>();
+            _client.BaseAddress = new Uri(apiUrl);
+        }
+
+        public CourseViewModel Course { get; set; }
+
+        public List<LessonViewModel> Exams { get; set; }
+
+        public async Task OnGetAsync(int id)
+        {
+            await Refresh(id);
+        }
+
+        private async Task Refresh(int id)
+        {
+            var request = new HttpRequestMessage(HttpMethod.Get, _client.BaseAddress + $"/course/" + id);
+            var res = await _client.SendAsync(request);
+            if (res.IsSuccessStatusCode)
+            {
+                var dataStr = await res.Content.ReadAsStringAsync();
+                var course = JsonConvert.DeserializeObject<CourseViewModel>(dataStr);
+                Course = course;
+                Exams = Course.Lessons.Where(lesson => lesson.IsExam).ToList();
+            }
         }
     }
 }
--- DrawClient/Pages/Admin/Course/List.cshtml.cs
+++ DrawClient/Pages/Admin/Course/List.cshtml.cs
@@ -1,12 +1,51 @@
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json;
+using ViewModel.Base;
+using ViewModel.Cart;
+using ViewModel.Course;
 
 namespace DrawClient.Pages.Admin.Course
 {
     public class ListModel : PageModel
     {
-        public void OnGet()
+        private readonly IConfiguration _configuration;
+        private readonly HttpClient _client;
+
+        public ListModel(IConfiguration configuration)
         {
+            _configuration = configuration;
+            _client = new HttpClient();
+            var apiUrl = _configuration.GetSection("ApiUrl").Get<string>();
+            _client.BaseAddress = new Uri(apiUrl);
+        }
+
+        public Page<CourseViewModel> Courses { get; set; }
+
+
+        public async Task<IActionResult> OnGetAsync(int pageIndex = 0)
+        {
+            await Refresh(pageIndex);
+            return Page();
+        }
+
+        private async Task Refresh(int pageIndex = 0)
+        {
+            var token = HttpContext.Session.GetString("learnerToken");
+            var request = new HttpRequestMessage(HttpMethod.Get, _client.BaseAddress + $"/course/page?pageIndex={pageIndex}&pageSize={6}");
+            request.Headers.Add("Authorization", $"Bearer {token}");
+
+            var res = await _client.SendAsync(request);
+            if (res.IsSuccessStatusCode)
+            {
+                var dataStr = await res.Content.ReadAsStringAsync();
+                var courses = JsonConvert.DeserializeObject<Page<CourseViewModel>>(dataStr);
+                if (courses is not null)
+                {
+                    Courses = courses;
+                }
+            }
         }
     }
 }
